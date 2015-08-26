6b1a91ad0cfb56610c4e348db6ecb5bebb8db78e
vicmars5 <mars.vic5@gmail.com>
2015-08-25 21:42:23 -0500

    Funciona ;)

--- Total a pagar de cinco articulos/main.cpp
+++ Total a pagar de cinco articulos/main.cpp
@@ -10,25 +10,23 @@ int main()
 
 	float articulo1 ,articulo2, articulo3, articulo4, articulo5, total;
 
+	std::cout << "Dame la cantidad de Manzanas" << std::endl;
+	std::cin >> articulo1;
 
+	std::cout << "Dame la cantidad de Sandwiches" << std::endl;
+	std::cin >> articulo2;
 
-	std::cout<<"Dame la cantidad de Manzanas"<<std::endl;
-	std::cin>>articulo1;
+	std::cout << "Dame la cantidad de Tacos" << std::endl;
+	std::cin >> articulo3;
 
-	std::cout<<"Dame la cantidad de Sandwiches"<<std::endl;
-	std::cin>>articulo2;
+	std::cout << "Dame la cantidad de Refresco" << std::endl;
+	std::cin >> articulo4;
 
-	std::cout<<"Dame la cantidad de Tacos"<<std::endl;
-	std::cin>>articulo3;
-
-	std::cout<<"Dame la cantidad de Refresco"<<std::endl;
-	std::cin>>articulo4;
-
-	std::cout<<"Dame la cantidad de Agua"<<std::endl;
-	std::cin>>articulo5;
+	std::cout << "Dame la cantidad de Agua" << std::endl;
+	std::cin >> articulo5;
 
 	total=(articulo1 * PRECIO1) + (articulo2 * PRECIO2) + (articulo3 * PRECIO3) + (articulo4 * PRECIO4) + (articulo5 * PRECIO5);
 
-	std::cout<<"Total a pagar "<<total;
-    return 0;
+	std::cout << "Total a pagar " << total;
+	return 0;
 }
